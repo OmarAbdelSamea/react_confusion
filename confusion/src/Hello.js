8d5e44b478639616480c3fdf7bea9938ac6cc0a8
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2020-08-19 19:27:33 +0200

    initial setup for confusion and test hello

---
+++ confusion/src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Hello() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Hello;
