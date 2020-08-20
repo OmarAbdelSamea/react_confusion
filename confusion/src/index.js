fc6058c3c2a178c44bf51f12ef8bf774ea9257bf
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2020-08-21 01:47:20 +0200

    Creating Menu component and adding it ot App.js

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navbar, NavbarBrand } from 'reactstrap';
 import './index.css';
 import App from './App';
-import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar dark color="info">
-      <div className="container">
-        <NavbarBrand href="/">React First</NavbarBrand>
-      </div>
-    </Navbar>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
