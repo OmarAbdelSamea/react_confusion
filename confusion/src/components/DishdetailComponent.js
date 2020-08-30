41e3757e9d261c3b20787049cc15ba754c61fd32
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2020-08-31 01:41:58 +0200

    Single page application

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,64 +1,75 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle} from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-    function RenderDish({dish}) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
-    function RenderComments({dish}) {
-        if (dish != null) {
-            const commentsList = dish.comments.map((comment) => {
-                return (
-                    <li>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                    </li>
-                );
-            })
-            return (
-                <div className="list-unstyled">
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">{commentsList}</ul>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
-
-    const DishDetail = (props) => {
+function RenderDish({ dish }) {
+    if (dish != null) {
         return (
+            <Card>
+                <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
+function RenderComments({ comments }) {
+    if (comments != null) {
+        const commentsList = comments.map((comment) => {
+            return (
+                <li>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                </li>
+            );
+        })
+        return (
+            <div className="list-unstyled">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">{commentsList}</ul>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
+
+const DishDetail = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
             <div className="row justify-content-center">
                 <div className="col-12 col-md-5 mt-3">
                     <RenderDish dish={props.dish} />
                 </div>
-
                 <div className="col-12 col-md-5 mt-3">
-                    <RenderComments dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
 
 export default DishDetail;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderMenuItem({ dish, onClick }) {
     return (
-        <Card onClick={() => onClick(dish.id)}>
-            <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -23,8 +26,20 @@ const Menu = (props) => {
     });
 
     return (
-        <div className="row justify-content-center">
-            {menu}
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                {menu}
+            </div>
         </div>
     );
 }
